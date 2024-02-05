15626db4c13299ada2796dcfdff43f0b8e25430c
Fantič <58391275+FantaFantic@users.noreply.github.com>
2024-02-05 19:13:10 +0100

    Cztenis druzstva content

--- wp-content/plugins/cztenis/includes/CZtenisDruzstva.php
+++ wp-content/plugins/cztenis/includes/CZtenisDruzstva.php
@@ -21,11 +21,11 @@ if (!class_exists('CZtenisDruzstva')) {
 
             public static function get_instance()
             {
-                if (null === self::$instance) {
-                    self::$instance = new self();
-                }
-        
-                return self::$instance;
+                  if (null === self::$instance) {
+                        self::$instance = new self();
+                  }
+
+                  return self::$instance;
             }
 
             public function __construct()
@@ -65,47 +65,51 @@ if (!class_exists('CZtenisDruzstva')) {
 
             function include_druzstvo_page()
             {
-                  // ob_start();
-                  // include MY_PLUGIN_PATH . 'includes/templates/druzstvo.php';
-                  // return ob_get_clean();
-
-                  $url = $this->get_druzstvo_url();
-
-                  // Check if the URL is valid
-                  if (filter_var($url, FILTER_VALIDATE_URL)) {
-                      $content = file_get_contents($url);
-          
-                      // Check if content retrieval was successful
-                      if ($content !== false) {
-                          $doc = new DOMDocument();
-                          libxml_use_internal_errors(true); // Disable libxml errors
-                          $doc->loadHTML($content);
-                          libxml_clear_errors(); // Clear any libxml errors
-          
-                          $xpath = new DOMXPath($doc);
-          
-                          // Find the div with class "span12"
-                          $span12Div = $xpath->query('//div[@class="span12"]')->item(0);
-          
-                          // Output the content of the found div
-                          if ($span12Div) {
-                              echo '<div class="span12">' . $doc->saveHTML($span12Div) . '</div>';
-                          } else {
-                              echo 'Div with class "span12" not found in the fetched content.';
-                          }
-                      } else {
-                          echo 'Error fetching content from URL.';
-                      }
-                  } else {
-                      echo 'Invalid URL.';
-                  }
-
-
+                  ob_start();
+                  include MY_PLUGIN_PATH . 'includes/templates/druzstvo.php';
+                  return ob_get_clean();
             }
 
             function get_druzstvo_url()
             {
                   return carbon_get_theme_option("cztenis_druzstva_link");
+            }
+
+            public function replace_links($content)
+            {
+                  // Replace links starting with "/"
+                  $content = preg_replace('/href="\/([^"]+)"/', 'href="https://www.cztenis.cz/$1" target="_blank"', $content);
+
+                  return $content;
+            }
+
+            public function extract_main_div($divContent)
+            {
+                  // Use a DOM parser to extract content within div.span12
+                  $doc = new DOMDocument();
+                  libxml_use_internal_errors(true);
+                  $doc->loadHTML($divContent);
+                  libxml_clear_errors();
+
+                  $xpath = new DOMXPath($doc);
+
+                  // Find the div with class "span12"
+                  $span12Div = $xpath->query('//div[@class="span12"]')->item(0);
+
+                  // If the div is found, get its innerHTML
+                  if ($span12Div) {
+                        return $doc->saveHTML($span12Div);
+                  } else {
+                        return 'Div with class "span12" not found in the fetched content.';
+                  }
+            }
+
+            function replace_image_links($content)
+            {
+                  // Replace image links with Dashicons and make them clickable
+                  $content = preg_replace('/<a\s+([^>]+)><img\s+([^>]+)><\/a>/', '<a $1><span class="dashicons dashicons-code-standards"></span><span style="display:none;">$2</span></a>', $content);
+
+                  return $content;
             }
       }
 
--- wp-content/plugins/cztenis/includes/templates/druzstvo.php
+++ wp-content/plugins/cztenis/includes/templates/druzstvo.php
@@ -1,11 +1,49 @@
 <?php
 
-if( !defined('ABSPATH') )
-{
+if (!defined('ABSPATH')) {
       die('Neoprávněný přístup!');
 }
 
 
 $cztenis_druzstva = CZtenisDruzstva::get_instance();
 
-echo $cztenis_druzstva->get_druzstvo_url();
+$url = $cztenis_druzstva->get_druzstvo_url();
+
+// Check if the URL is valid
+if (filter_var($url, FILTER_VALIDATE_URL)) {
+      $content = file_get_contents($url);
+
+      // Check if content retrieval was successful
+      if ($content !== false) {
+            $content = file_get_contents($url);
+
+            // Check if content retrieval was successful
+            if ($content !== false) {
+                // Extract content from within the div.span12
+                $divContent = $cztenis_druzstva->extract_main_div($content);
+
+                // Replace links in the extracted content
+                $divContent = $cztenis_druzstva->replace_links($divContent);
+                $divContent = $cztenis_druzstva->replace_image_links($divContent);
+
+                // Output the modified content within the div.span12
+                echo '<div class="span12">' . $divContent . '</div>';
+            } else {
+                  echo 'Nenalezeno.';
+            }
+      } else {
+            echo 'Nenalezeno.';
+      }
+} else {
+      echo 'Neexistující URL.';
+}
+
+
+?>
+
+<style>
+      div.span12 a img {
+            border: 0;
+            display: none;
+      }
+</style>
